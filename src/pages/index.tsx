2fce38acbf15ede3f674db9f007b68d10cb7fd4c
Zaela Rose <zaela.r@tamakicontrol.com>
2025-06-06 09:00:31 -0500

    Switch to yarn to avoid build errors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,7 +48,7 @@ function HomepageHeader() {
 					<Link
 					    className="button button--primary button--lg mainButton"
 						style={{marginTop: 1 + 'rem'}}
-						to='/Issues/intro'
+						to='/issues/intro'
 					>
 						Report an Issue
 					</Link>
